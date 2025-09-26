858cf8b916058daa00b5ad94b239cb4bf25d3b64
htuan2800 <huynhngoctuan48@gmail.com>
2025-09-26 10:57:47 +0700

    first commit

---
+++ src/test/java/com/vcbackend/VcbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
